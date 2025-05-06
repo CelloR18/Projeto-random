3e6ef9839d5f637c9c7242928adb4d669308b98e
CelloR18 <gustavosq2020@hotmail.com>
2025-05-05 21:21:13 -0300

    responsividade

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function cliquei(){
-    const valor1= Math.ceil(document.querySelector(".primeiro").value)
-    const valor2= Math.floor(document.querySelector(".segundo").value)
-    const result= Math.floor(Math.random() * (valor1 - valor2 + 1)) + valor2;
+    const valor1= Math.ceil(document.querySelector(".primeiro").value) //coloque tudo no Math.ceil
+    const valor2= Math.floor(document.querySelector(".segundo").value) //coloque tudo no Math.floor
+    const result= Math.floor(Math.random() * (valor1 - valor2 + 1)) + valor2; // para gerar um numero aletorio
     
     alert(result)
 }
