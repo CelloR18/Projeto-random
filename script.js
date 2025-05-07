1be9fd9ef4b54142a652d6347952ef483abf0b40
CelloR18 <gustavosq2020@hotmail.com>
2025-05-06 21:24:32 -0300

    função menor ou igual acrescentada

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ function cliquei(){
     const valor2= Math.floor(document.querySelector(".segundo").value) //coloque tudo no Math.floor
     const result= Math.floor(Math.random() * (valor1 - valor2 + 1)) + valor2; // para gerar um numero aletorio
     
-    alert(result)
+    if(valor1 >= valor2){
+        alert("O primeiro valor deve ser menor que o segundo valor")
+    }
+    else{
+        alert(result)
+    }
+
+    
 }
